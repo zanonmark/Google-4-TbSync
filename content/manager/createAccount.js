62f87c180807528dd55ca9e421e6b204cd007718
Marco Zanon <info@marcozanon.com>
2021-01-07 19:24:23 +0100

    Fixed copyright headers.

--- content/manager/createAccount.js
+++ content/manager/createAccount.js
@@ -1,12 +1,13 @@
 /*
- * This file is part of GOOGLE-4-TbSync.
+ * This file is part of Google-4-TbSync.
+ * See CONTRIBUTORS.md for details.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
  */
  
- "use strict";
+"use strict";
 
 var { TbSync } = ChromeUtils.import("chrome://tbsync/content/tbsync.jsm");
 
--- content/manager/editAccountOverlay.js
+++ content/manager/editAccountOverlay.js
@@ -1,5 +1,6 @@
 /*
- * This file is part of GOOGLE-4-TbSync.
+ * This file is part of Google-4-TbSync.
+ * See CONTRIBUTORS.md for details.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
