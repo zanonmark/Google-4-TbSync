b059e0a956de00f5324e71f94f60dd9b70b2f083
Marco Zanon <info@marcozanon.com>
2021-01-14 23:01:54 +0100

    Implemented buttons to renew the access token.

---
+++ content/includes/PeopleAPI.js
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Google-4-TbSync.
+ * See CONTRIBUTORS.md for details.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+
+"use strict";
+
+const API_KEY = "AIzaSyAsE_GvLvP_wAFSc_GjgMO3RI09Cz2aQbY";
+const DISCOVERY_DOCS = [ "https://people.googleapis.com/$discovery/rest?version=v1" ];
+const CLIENT_ID = "307111179231-m9mhci0rl9f02tqbm20rgca9hk61leg4.apps.googleusercontent.com";
+const SCOPES = "profile https://www.googleapis.com/auth/contacts";
+
+class PeopleAPI {
+
+    constructor() { // TODO
+    }
+
+    getNewAccessToken() { // TODO
+    }
+
+}
+
+let peopleApi = new PeopleAPI();
