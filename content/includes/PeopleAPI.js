c8a025df729066d898bd0c98345ffa70f31496e5
Marco Zanon <info@marcozanon.com>
2021-01-19 19:25:06 +0100

    Implemented getAccessToken() and fixed getNewCode().

--- content/includes/PeopleAPI.js
+++ content/includes/PeopleAPI.js
@@ -17,7 +17,6 @@ class PeopleAPI {
     clientID = null;
     clientSecret = null;
     code = null;
-    accessToken = null;
 
     constructor(clientID, clientSecret, code) {
         if ((null == clientID) || ("" === clientID)) {
@@ -51,10 +50,11 @@ class PeopleAPI {
         if (null == codeWidget) {
             throw "Invalid value: codeWidget: null."
         }
-        // Prepare the authorization request URL.
-        let authorizationRequestURL = "https://accounts.google.com/o/oauth2/auth?client_id=__CLIENT_ID__&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=__SCOPE__&response_type=code".replace("__CLIENT_ID__", this.getClientID()).replace("__SCOPE__", SCOPES);
+        // Prepare the authorization code request URL.
+        let authorizationCodeRequestURL = "https://accounts.google.com/o/oauth2/auth?client_id=__CLIENT_ID__&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=__SCOPE__&response_type=code".replace("__CLIENT_ID__", this.getClientID()).replace("__SCOPE__", SCOPES);
+        console.log("PeopleAPI.getNewCode(): authorizationCodeRequestURL = " + authorizationCodeRequestURL);
         // Load the URL.
-        browserWidget.src = authorizationRequestURL;
+        browserWidget.src = authorizationCodeRequestURL;
         browserWidget.style.display = "block";
         // Check the response every 1s.
         let interval = setInterval(function() {
@@ -68,21 +68,59 @@ class PeopleAPI {
                     let code = group[1];
                     // ...apply the code to the textbox...
                     codeWidget.value = code;
-                    // ...trigger the onInput event if necessary...
+                    console.log("PeopleAPI.getNewCode(): code = " + code);
+                    // ...trigger the onInput event (only if necessary)...
                     if (codeWidget.oninput) {
                         codeWidget.oninput();
                     }
                     // ...hide the browser...
                     browserWidget.style.display = "none";
                     browserWidget.src = "about:blank";
-                    // ...and stop the interval.
+                    // ...stop the interval...
                     clearInterval(interval);
+                    // ...and set the code to the object.
+                    this.code = code;
                 }
             }
         }, 1000);
     }
 
     getAccessToken() {
+        // Prepare the access token request URL and data.
+        let accessTokenRequestURL = "https://accounts.google.com/o/oauth2/token";
+        let accessTokenRequestData = {
+            code: this.getCode(),
+            client_id: this.getClientID(),
+            client_secret: this.getClientSecret(),
+            redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+            grant_type: "authorization_code",
+        };
+        console.log("PeopleAPI.getAccessToken(): accessTokenRequestURL = " + accessTokenRequestURL);
+        console.log("PeopleAPI.getAccessToken(): accessTokenRequestData = " + JSON.stringify(accessTokenRequestData));
+        // Perform the request.
+        fetch(accessTokenRequestURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(accessTokenRequestData),
+        }).then(function(response) {
+            if (200 != response.status) {
+                throw "Invalid response: " + response.status
+            }
+            //
+            response.json().then(function(data) {
+                let accessToken = data.access_token;
+                console.log("PeopleAPI.getAccessToken(): accessToken = " + accessToken);
+                //
+                return accessToken;
+            });
+        });
+    }
+
+    getContactList() {
+        // Get a valid access token.
+        let accessToken = this.getAccessToken();
 // TODO
     }
 
