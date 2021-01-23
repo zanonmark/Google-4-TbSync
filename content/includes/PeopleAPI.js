2325df9664f56c7954ced494e87e8dcf8769e8a7
Marco Zanon <info@marcozanon.com>
2021-01-23 15:49:48 +0100

    Renamed 'code' to 'authorization code'.

--- content/includes/PeopleAPI.js
+++ content/includes/PeopleAPI.js
@@ -40,18 +40,18 @@ class PeopleAPI {
 
     /* Connections. */
 
-    async getNewCode() {
+    async getNewAuthorizationCode() {
         let clientID = this.getClientID();
         // Prepare a new promise.
         let promise = new Promise(function(resolve, reject) {
             // Prepare the authorization code request URL.
             let authorizationCodeRequestURL = "https://accounts.google.com/o/oauth2/auth?client_id=__CLIENT_ID__&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=__SCOPE__&response_type=code".replace("__CLIENT_ID__", clientID).replace("__SCOPE__", SCOPES);
-            console.log("PeopleAPI.getNewCode(): authorizationCodeRequestURL = " + authorizationCodeRequestURL);
+            console.log("PeopleAPI.getNewAuthorizationCode(): authorizationCodeRequestURL = " + authorizationCodeRequestURL);
             // Open the browser window.
             let authenticationWindow = window.open("chrome://google-4-tbsync/content/manager/authenticate.xhtml", null, "chrome");
             let browserWidget = null;
             let titleInterval = null;
-            let codeRetrieved = false;
+            let authorizationCodeRetrieved = false;
             authenticationWindow.onload = function() {
                 // Set the browser widget.
                 browserWidget = authenticationWindow.document.getElementById("browser");
@@ -65,17 +65,17 @@ class PeopleAPI {
                     if (browserTitle.startsWith("Success")) {
                         let pattern = new RegExp("code=(.*)&", "i");
                         let group = pattern.exec(browserTitle);
-                        // ...and if the code could be retrieved...
+                        // ...and if the authorization code could be retrieved...
                         if (null != group) {
-                            let code = group[1];
-                            console.log("PeopleAPI.getNewCode(): code = " + code);
+                            let authorizationCode = group[1];
+                            console.log("PeopleAPI.getNewAuthorizationCode(): authorizationCode = " + authorizationCode);
                             // ...close the browser window...
                             authenticationWindow.close();
                             // ...stop the title interval...
                             clearInterval(titleInterval);
-                            // ...and return the code.
-                            codeRetrieved = true;
-                            resolve(code);
+                            // ...and return the authorization code.
+                            authorizationCodeRetrieved = true;
+                            resolve(authorizationCode);
                         }
                     }
                 }, 1000);
@@ -83,9 +83,9 @@ class PeopleAPI {
             authenticationWindow.onclose = function() {
                 // Stop the title interval.
                 clearInterval(titleInterval);
-                // Return an error if the browser window was closed before retrieving the code.
-                if (!codeRetrieved) {
-                    reject(new Error("Browser window closed before the code was retrieved."));
+                // Return an error if the browser window was closed before retrieving the authorization code.
+                if (!authorizationCodeRetrieved) {
+                    reject(new Error("Browser window closed before the authorization code was retrieved."));
                 }
             }
         });
@@ -94,12 +94,12 @@ class PeopleAPI {
     }
 
     async getNewAccessToken() {
-        // Get a new code.
-        let code = await this.getNewCode();
+        // Get a new authorization code.
+        let authorizationCode = await this.getNewAuthorizationCode();
         // Prepare the access token request URL and data.
         let accessTokenRequestURL = "https://accounts.google.com/o/oauth2/token";
         let accessTokenRequestData = {
-            code: code,
+            code: authorizationCode,
             client_id: this.getClientID(),
             client_secret: this.getClientSecret(),
             redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
