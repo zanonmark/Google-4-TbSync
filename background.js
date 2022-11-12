7b2f37005fc1cbd876c8aef54dfc36da323de4cf
Marco Zanon <info@marcozanon.com>
2022-11-12 18:53:09 +0100

    Fixed coding style.

--- background.js
+++ background.js
@@ -23,14 +23,14 @@ async function main() {
     //
     await messenger.BootstrapLoader.registerBootstrapScript("chrome://google-4-tbsync/content/bootstrap.js");
     //
-    messenger.contacts.onCreated.addListener(function(node, id) { addressBookEventManager.onContactCreated(node, id); });
-    messenger.contacts.onUpdated.addListener(function(node, changedProperties) { addressBookEventManager.onContactUpdated(node, changedProperties); });
-    messenger.contacts.onDeleted.addListener(function(parentId, id) { addressBookEventManager.onContactDeleted(parentId, id); });
-    messenger.mailingLists.onCreated.addListener(function(node) { addressBookEventManager.onMailingListCreated(node); });
-    messenger.mailingLists.onUpdated.addListener(function(node) { addressBookEventManager.onMailingListUpdated(node); });
-    messenger.mailingLists.onDeleted.addListener(function(parentId, id) { addressBookEventManager.onMailingListDeleted(parentId, id); });
-    messenger.mailingLists.onMemberAdded.addListener(function(node) { addressBookEventManager.onMailingListMemberAdded(node); });
-    messenger.mailingLists.onMemberRemoved.addListener(function(parentId, id) { addressBookEventManager.onMailingListMemberRemoved(parentId, id); });
+    messenger.contacts.onCreated.addListener( (node, id) => addressBookEventManager.onContactCreated(node, id) );
+    messenger.contacts.onUpdated.addListener( (node, changedProperties) => addressBookEventManager.onContactUpdated(node, changedProperties) );
+    messenger.contacts.onDeleted.addListener( (parentId, id) => addressBookEventManager.onContactDeleted(parentId, id) );
+    messenger.mailingLists.onCreated.addListener( (node) => addressBookEventManager.onMailingListCreated(node) );
+    messenger.mailingLists.onUpdated.addListener( (node) => addressBookEventManager.onMailingListUpdated(node) );
+    messenger.mailingLists.onDeleted.addListener( (parentId, id) => addressBookEventManager.onMailingListDeleted(parentId, id) );
+    messenger.mailingLists.onMemberAdded.addListener( (node) => addressBookEventManager.onMailingListMemberAdded(node) );
+    messenger.mailingLists.onMemberRemoved.addListener( (parentId, id) => addressBookEventManager.onMailingListMemberRemoved(parentId, id) );
 }
 
 main();
